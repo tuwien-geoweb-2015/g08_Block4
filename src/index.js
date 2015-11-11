dda3cf6c59e5542d5d07d0cdac5781436996b664
vanperste <luki.persterer@gmx.at>
2015-11-11 17:02:44 +0100

    newer
    new

--- src/index.js
+++ src/index.js
@@ -16,3 +16,21 @@ var map = new ol.Map({
     zoom: 13
   })
 });
+
+var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
+var layer = 'feedback';
+var prefix = 'ifip_2015'; // Ersetzen durch euren Arbeitsbereich-Namen
+var featureNS = 'http://ifip/2015'; // Ersetzen durch eure Namensraum-URI
+var form = document.getElementById('feedback');
+
+var feature = new ol.Feature();
+feature.setGeometryName('geom');
+feature.setGeometry(new ol.geom.Point(map.getView().getCenter()));
+
+var send = require('./feedback_send');
+send(form, feature, url, {
+  featureType: layer,
+  featurePrefix: prefix,
+  featureNS: featureNS,
+  srsName: map.getView().getProjection().getCode()
+});
