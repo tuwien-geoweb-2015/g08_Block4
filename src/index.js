214fcf2940ad28a43c616dca9099bb503cfb6d77
Hoebi <hoebi.handy@gmail.com>
2015-11-12 10:41:23 +0100

    12-11-fruh

--- src/index.js
+++ src/index.js
@@ -33,4 +33,21 @@ send(form, feature, url, {
   featurePrefix: prefix,
   featureNS: featureNS,
   srsName: map.getView().getProjection().getCode()
+  
+  var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
+var layer = 'feedback';
+var prefix = 'ifip_2015'; // Ersetzen durch euren Arbeitsbereich-Namen
+var featureNS = 'http://ifip/2015'; // Ersetzen durch eure Namensraum-URI
+var form = document.getElementById('feedback');
+
+var feature = new ol.Feature();
+feature.setGeometryName('geom');
+feature.setGeometry(new ol.geom.Point(map.getView().getCenter()));
+
+var send = require('./feedback_send');
+send(form, feature, url, {
+  featureType: layer,
+  featurePrefix: prefix,
+  featureNS: featureNS,
+  srsName: map.getView().getProjection().getCode()
 });
